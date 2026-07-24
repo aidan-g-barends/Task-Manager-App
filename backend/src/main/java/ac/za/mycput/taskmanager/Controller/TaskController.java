ed8dc8b4aa7a1075df71e74042a921228ee07bba
aidan-g-barends <aidanbarends95@gmail.com>
2026-07-24 16:46:09 +0200

    Added a taskController and its CRUD methods + its extra added mtods

--- backend/src/main/java/ac/za/mycput/taskmanager/Controller/TaskController.java
+++ backend/src/main/java/ac/za/mycput/taskmanager/Controller/TaskController.java
@@ -1,4 +1,57 @@
-package ac.za.mycput.taskmanager.Controller;
+    package ac.za.mycput.taskmanager.Controller;
 
-public class TaskController {
-}
+    import ac.za.mycput.taskmanager.Domain.Task;
+    import ac.za.mycput.taskmanager.Domain.User;
+    import ac.za.mycput.taskmanager.Service.TaskService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.*;
+
+    import java.util.List;
+
+    @RestController
+    @RequestMapping("/api/task")
+    public class TaskController {
+
+        private final TaskService taskService;
+
+        @Autowired
+        public TaskController(TaskService taskService){
+            this.taskService = taskService;
+        }
+
+        @PostMapping("/create")
+        public Task create(@RequestBody Task task){
+            return taskService.create(task);
+        }
+        @GetMapping("/read/{id}")
+        public Task read(@PathVariable Long id){
+            return taskService.read(id);
+        }
+        @PutMapping("/update")
+        public Task update(@RequestBody Task task){
+            return taskService.update(task);
+        }
+
+        @DeleteMapping("/delete/{id}")
+        public boolean delete(@PathVariable Long id){
+            taskService.delete(id);
+            return false;
+        }
+
+        @GetMapping("/getAll")
+        public List<Task> getAll(){
+            return taskService.getAll();
+        }
+
+        @GetMapping("/title/{title}")
+        public Task findByTitle(@PathVariable String title){
+            return taskService.findByTitle(title);
+        }
+
+        @GetMapping("/user/{user}")
+        public List<Task> findByUser(@PathVariable User user){
+            return taskService.findByUser(user);
+
+        }
+
+    }
