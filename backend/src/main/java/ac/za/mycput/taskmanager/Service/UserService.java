1f1c2e1aecf1b6e9ced9d495317a2721f1c1d640
aidan-g-barends <aidanbarends95@gmail.com>
2026-07-27 09:54:55 +0200

    Complete backend CRUD, entitites, factory, repository, services, controllers

--- backend/src/main/java/ac/za/mycput/taskmanager/Service/UserService.java
+++ backend/src/main/java/ac/za/mycput/taskmanager/Service/UserService.java
@@ -4,9 +4,11 @@ import ac.za.mycput.taskmanager.Domain.User;
 import ac.za.mycput.taskmanager.Repository.UserRepository;
 import ac.za.mycput.taskmanager.Service.impl.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements IUserService {
 
     private UserRepository userRepository;
