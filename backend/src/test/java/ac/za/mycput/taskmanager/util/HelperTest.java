5d5a3f891f9c6cd987c84f10b61ab07475982459
aidan-g-barends <aidanbarends95@gmail.com>
2026-07-29 12:25:23 +0200

    Added more tests and they all are successfull

--- backend/src/test/java/ac/za/mycput/taskmanager/util/HelperTest.java
+++ backend/src/test/java/ac/za/mycput/taskmanager/util/HelperTest.java
@@ -2,6 +2,8 @@ package ac.za.mycput.taskmanager.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -15,5 +17,40 @@ class HelperTest {
     @Test
     void isValidEmail_returnsFalse_forMissingAtSymbol() {
         assertFalse(Helper.isValidEmail("notanemail"));
+    }
+
+    @Test
+    void isNullOrEmpty_returnsTrue_forNull() {
+        assertTrue(Helper.isNullOrEmpty(null));
+    }
+
+    @Test
+    void isNullOrEmpty_returnsTrue_forEmptyString() {
+        assertTrue(Helper.isNullOrEmpty(""));
+    }
+
+    @Test
+    void isNullOrEmpty_returnsFalse_forNormalString() {
+        assertFalse(Helper.isNullOrEmpty("hello"));
+    }
+
+    @Test
+    void isValidDate_returnTrue_forValidDate(){
+        assertTrue(Helper.isValidDate(LocalDate.now()));
+    }
+
+    @Test
+    void isValidDate_returnFalse_forInvalidDate(){
+        assertFalse(Helper.isValidDate(null));
+    }
+
+    @Test
+    void isValidDate_returnsFalse_forPastDate() {
+        assertFalse(Helper.isValidDate(LocalDate.now().minusDays(1)));
+    }
+
+    @Test
+    void isValidDate_returnsTrue_forFutureDate() {
+        assertTrue(Helper.isValidDate(LocalDate.now().plusDays(1)));
     }
 }
