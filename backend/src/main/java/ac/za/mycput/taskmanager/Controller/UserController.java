a3c73e5a9335a902e972ada6752a7356b26835a9
aidan-g-barends <aidanbarends95@gmail.com>
2026-07-31 10:04:20 +0200

    added validation to the create and update endpoints

--- backend/src/main/java/ac/za/mycput/taskmanager/Controller/UserController.java
+++ backend/src/main/java/ac/za/mycput/taskmanager/Controller/UserController.java
@@ -2,8 +2,8 @@ package ac.za.mycput.taskmanager.Controller;
 
 
 import ac.za.mycput.taskmanager.Domain.User;
-import ac.za.mycput.taskmanager.Service.UserService;
 import ac.za.mycput.taskmanager.Service.impl.IUserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,28 +21,23 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public User create(@RequestBody User user){
+    public User create(@Valid @RequestBody User user){
         return userService.create(user);
     }
-
-
 
     @GetMapping("/read/{id}")
     public User read(@PathVariable Long id){
         return userService.read(id);
     }
 
-
     @PutMapping("/update")
-    public User update(@RequestBody User user){
+    public User update(@Valid @RequestBody User user){
         return userService.update(user);
     }
-
 
     @DeleteMapping("/delete/{id}")
     public boolean delete(@PathVariable Long id){
         return userService.delete(id);
-
     }
 
     @GetMapping("/email/{email}")
