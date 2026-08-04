41a92081276f0e6dbdbdccd74bdc1030a1c89efe
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-04 14:14:02 +0200

    updated task.ts

--- frontend/src/app/services/task.ts
+++ frontend/src/app/services/task.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+  dueDate: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Task {}
+export class TaskService {
+  private readonly apiUrl = 'http://localhost:8080/api/task';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/getAll`);
+  }
+}
