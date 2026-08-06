1368d7c6de86223bf92834fbd948309c03cb347f
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-06 09:34:40 +0200

    feat: complete UserList component and wire into app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskList } from './components/task-list/task-list';
 import { TaskForm } from './components/task-form/task-form';
+import { UserList } from './components/user-list/user-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskList, TaskForm],
+  imports: [RouterOutlet, TaskList, TaskForm, UserList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
