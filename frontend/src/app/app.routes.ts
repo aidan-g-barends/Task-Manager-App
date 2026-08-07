480fa866b9f7b2244cc3d7209667dc64f4815d73
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-07 10:38:54 +0200

    configure app routes for taskList, TaskForm and userList

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TaskList } from './components/task-list/task-list';
+import { TaskForm } from './components/task-form/task-form';
+import { UserList } from './components/user-list/user-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskList },
+  { path: 'tasks/new', component: TaskForm },
+  { path: 'users', component: UserList },
+];
