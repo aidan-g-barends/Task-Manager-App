e37a075427c8370ffae93ec155f45ff1e5505724
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-07 10:51:01 +0200

    added the completed route

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Routes } from '@angular/router';
+import { TaskList } from './components/task-list/task-list';
+import { TaskForm } from './components/task-form/task-form';
+import { UserList } from './components/user-list/user-list';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('frontend');
-}
+export const routes: Routes = [
+  { path: '', component: TaskList },
+  { path: 'completed', component: TaskList, data: { completedOnly: true } },
+  { path: 'tasks/new', component: TaskForm },
+  { path: 'users', component: UserList },
+];
