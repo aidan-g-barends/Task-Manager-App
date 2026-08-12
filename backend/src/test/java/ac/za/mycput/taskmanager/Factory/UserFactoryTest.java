ced8f47ff9842cb29e096a660225cc0d3a13b3fa
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-12 10:19:45 +0200

    update userFactory for new password parameter

--- backend/src/test/java/ac/za/mycput/taskmanager/Factory/UserFactoryTest.java
+++ backend/src/test/java/ac/za/mycput/taskmanager/Factory/UserFactoryTest.java
@@ -11,25 +11,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class UserFactoryTest {
 
-   @Test
+    @Test
     void createValidUser(){
-       User user = UserFactory.createUser("Aidan", "aidan@gmail.com", null);
+        User user = UserFactory.createUser("Aidan", "aidan@gmail.com", "TestPass123!", null);
 
-       assertEquals("Aidan", user.getName());
-       assertEquals("aidan@gmail.com", user.getEmail());
-   }
+        assertEquals("Aidan", user.getName());
+        assertEquals("aidan@gmail.com", user.getEmail());
+    }
 
-   @Test
-   void createUser_whenNameIsNullOrEmpty(){
-       assertThrows(IllegalArgumentException.class, () -> {
-           UserFactory.createUser("", "aidan@gmail.com", null);
-       });
-   }
+    @Test
+    void createUser_whenNameIsNullOrEmpty(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserFactory.createUser("", "aidan@gmail.com", "TestPass123!", null);
+        });
+    }
 
     @Test
     void createUser_throwsException_whenEmailIsInvalid() {
         assertThrows(IllegalArgumentException.class, () -> {
-            UserFactory.createUser("Aidan", "notanemail", null);
+            UserFactory.createUser("Aidan", "notanemail", "TestPass123!", null);
+        });
+    }
+
+    @Test
+    void createUser_throwsException_whenPasswordIsBlank() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserFactory.createUser("Aidan", "aidan@gmail.com", "", null);
         });
     }
 }
