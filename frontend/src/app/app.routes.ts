57eff1065b05032d4d57ac0fd915a2261f5ffb69
aidan-g-barends <aidanbarends95@gmail.com>
2026-08-13 09:05:29 +0200

    add missing task detail/edit and user create/edit routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TaskList } from './components/task-list/task-list';
 import { TaskForm } from './components/task-form/task-form';
+import { TaskDetail } from './components/task-detail/task-detail';
 import { UserList } from './components/user-list/user-list';
+import { UserForm } from './components/user-form/user-form';
 import { Login } from './components/login/login';
 
 export const routes: Routes = [
@@ -9,5 +11,9 @@ export const routes: Routes = [
   { path: '', component: TaskList },
   { path: 'completed', component: TaskList, data: { completedOnly: true } },
   { path: 'tasks/new', component: TaskForm },
+  { path: 'tasks/:id', component: TaskDetail },
+  { path: 'tasks/:id/edit', component: TaskForm },
   { path: 'users', component: UserList },
+  { path: 'users/new', component: UserForm },
+  { path: 'users/:id/edit', component: UserForm },
 ];
